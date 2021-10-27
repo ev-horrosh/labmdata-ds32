e28a4a777be8ce4eaa87dbed24f2d73ac48139c1
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2021-10-26 21:33:53 -0400

    4

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # lambdata-ds32
+
+Lambda school unit 3 sprint 1 assignment
--- helper_functions.py
+++ helper_functions.py
@@ -9,16 +9,16 @@ def null_count(df):
 def split_dates(date_series):
     df = pd.DataFrame(date_series)
     df[0] = pd.to_datetime(df[0])
-    #     # df['column']
     for d in df:
         df["month"] = df[0].dt.month
         df["day"] = df[0].dt.day
-        df["year"] = df[0].dt.year
         df = df.drop(columns=0)
     return df
 
 
 def list_2_series(list_2_series, df):
+
     series = pd.Series(list_2_series)
-    df = pd.DataFrame(series)
+
+ df = pd.DataFrame(series)
     return df
