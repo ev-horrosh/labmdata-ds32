0719b3466b2938a8057ae099f5907013657d7a33
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2021-10-27 02:55:32 -0400

    test

--- helper_functions.py
+++ helper_functions.py
@@ -5,20 +5,32 @@ import numpy as np
 def null_count(df):
     return df.isnull().sum().sum()
 
+class SplitDates:
+    '''
+    Splits datatime into month, day, year.
+    Takes a datetime series column,
+    splits it and gets rid of original column
+    '''
 
-def split_dates(date_series):
-    df = pd.DataFrame(date_series)
-    df[0] = pd.to_datetime(df[0])
-    for d in df:
-        df["month"] = df[0].dt.month
-        df["day"] = df[0].dt.day
-        df = df.drop(columns=0)
-    return df
+    def __init__(self, date_series):
+        self.date_series=date_series
+        
+    def split_dates(self.date_series):
+
+        df = pd.DataFrame(self.date_series)
+        df[0] = pd.to_datetime(df[0])
+        for d in df:
+            df["month"] = df[0].dt.month
+            df["day"] = df[0].dt.day
+            df = df.drop(columns=0)
+        return df
 
 
 def list_2_series(list_2_series, df):
-
     series = pd.Series(list_2_series)
-
- df = pd.DataFrame(series)
+    df = pd.DataFrame(series)
     return df
+
+
+if __name__=='__main__':
+    
