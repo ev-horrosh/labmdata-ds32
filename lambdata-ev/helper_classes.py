0fdff0b7474852cb739f5d5e19063fc424b00413
Evgeny Khoroshukhin <horoshuhin@gmail.com>
2021-10-28 02:54:15 -0400

    added docker image that takes data and outputs dataframe

--- lambdata-ev/helper_classes.py
+++ lambdata-ev/helper_classes.py
@@ -3,8 +3,6 @@ import numpy as np
 
 
 class Helper:
-   
-
     def __init__(self, df):
         self.df = df
 
@@ -33,16 +31,23 @@ class Helper:
         return df.isnull().sum().sum()
 
 
-# def list_2_series(list_2_series, df):
-#     series = pd.Series(list_2_series)
-#     df = pd.DataFrame(series)
-#     return df
+class List2Series:
+    def __init__(self, list_2_series):
+        self.list_2_series = list_2_series
+
+    def list_2_series(list_2_series):
+        series = pd.Series(list_2_series)
+        df = pd.DataFrame(series, columns=["your list"])
+        return df
+
 
 if __name__ == "__main__":
-
     # TESTING
-    # df = Helper.split_dates(pd.Series(["1/12/2006", "2/10/2017"]))
+    user_input = (input("provide somethin: ")).lower().split(",")
+    df = List2Series.list_2_series(user_input)
 
+    # df = Helper.split_dates(pd.Series(["1/12/2006", "2/10/2017"]))
     # df = Helper.null_count(pd.Series(["1/12/2006", "2/10/2017", np.nan]))
     # help(Helper.null_count)
-    # print(df)
+
+    print(df)
